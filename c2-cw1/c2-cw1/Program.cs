cf952f5075e2e88d916fbf46bc141fdee9e23f5e
moathkwt <100304331+moathkwt@users.noreply.github.com>
2022-03-05 11:53:18 +0300

    code

---
+++ c2-cw1/c2-cw1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace c2_cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "clash";
+
+            double characterspeed = 1.50;
+
+            int characterage = 17;
+
+            string superpower1 = "breakwood";
+            string superpower2 = "breakwalls";
+
+            
+
+            Console.WriteLine(" my name is " + characterName + " my age is " + characterage + " my superpower is " + superpower1 + " my second superpower is " + " and my speed is " + characterspeed );
+        }
+    }
+}
